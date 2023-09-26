9666860d3ff2b5eb6fddf340dca8086b526d1cfb
Elmas <elmasdedic290@gmail.com>
2023-09-26 09:54:55 +0200

    --

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $url = $row['url'];
-        echo "<div class='image'><img src='$url' alt='Image'></div>";
+        echo "<img src='$url' alt='Image'>";
     }
 } else {
     echo "No images found.";
