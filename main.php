6d785167c3f570f4efb901db38e3eb02dee95c88
Elmas <elmasdedic290@gmail.com>
2023-10-03 10:11:32 +0200

    10.11

--- main.php
+++ main.php
@@ -1,13 +1,14 @@
 <?php
 include "connection.php";
 
-$sql = "SELECT url FROM pini";
+$sql = "SELECT * FROM pini";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $url = $row['url'];
-        echo "<a href=".'view.php'."><img src='$url' alt='Image'>";
+        $id = $row['id'];        
+        echo "<a href='view.php?id=$id'><img src='$url' alt='Image'></a>";
     }
 } else {
     echo "No images found.";
